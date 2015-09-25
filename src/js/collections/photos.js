8c155d66e15e8943db204ad1da20be313d01c3af
Vijaya Anand <vijay.prideparrot@gmail.com>
2015-09-25 11:49:41 +0530

    Renamed comment in photos collection

--- src/js/collections/photos.js
+++ src/js/collections/photos.js
@@ -2,26 +2,26 @@ define(['backbone', 'models/photo', 'backbonelocalstorage'], function (Backbone,
 
   'use strict';
 
-  // Represent collection of photos. 
+  // Represent collection of photos.
   var Photos = Backbone.Collection.extend({
 
     // Use localstorage for persistence.
     localStorage: new Backbone.LocalStorage('secret-photos'),
 
     model: Photo,
-    
+
     // Sort the photos in descending order by 'lastSaved' date.
     comparator: function(photo) {
-      var lastSavedDate = new Date(photo.get('lastSaved')); 
+      var lastSavedDate = new Date(photo.get('lastSaved'));
       return -lastSavedDate.getTime();
     },
 
-    // Wild card search by 'description'.
+    // Text search by 'description'.
     search: function (text) {
       if(!text) {
         return this.models;
       }
-      
+
       return this.filter(function (photo) {
         return photo.get('description').toLowerCase().indexOf(text.toLowerCase()) > -1;
       });
